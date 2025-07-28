4a6ba55ae6fe1e3e28a75adc941b4a9a94b207be
Maximilian Canez Fernandes <92341177+MaximilianCF@users.noreply.github.com>
2025-07-27 22:15:07 -0300

    Update nixos-options-fzf.sh

--- scripts/nixos-options-fzf.sh
+++ scripts/nixos-options-fzf.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-# Hal-powered fuzzy finder for NixOS & Home Manager options
-
-QUERY=$(curl -s https://search.nixos.org/options/search?q= | jq '.options')
-HM_QUERY=$(curl -s https://nix-community.github.io/home-manager/options.json)
-
 TMP=$(mktemp)
 
 (
   echo "# NixOS Options"
-  curl -s "https://search.nixos.org/options/search.json?query=" | jq -r '.options[] | "\(.name)\t\(.description)"'
+  curl -s "https://search.nixos.org/options/search.json?query=" |
+    jq -r '.options[] | "\(.name)\t\(.description)"'
+
   echo "# Home Manager Options"
-  curl -s "https://nix-community.github.io/home-manager/options.json" | jq -r '.[] | "\(.name)\t\(.description)"'
+  curl -s "https://nix-community.github.io/home-manager/options.json" |
+    jq -r '.[] | "\(.name)\t\(.description)"'
 ) > "$TMP"
 
 fzf --with-nth=1,2 \
@@ -21,4 +19,3 @@ fzf --with-nth=1,2 \
     --height=40% --border --ansi
 
 rm "$TMP"
-
