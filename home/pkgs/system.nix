e469f094887a9d942cc975d2d4a4b33da2261ff7
Maximilian Canez Fernandes <92341177+MaximilianCF@users.noreply.github.com>
2025-07-27 23:00:28 -0300

    Update system.nix

--- home/pkgs/system.nix
+++ home/pkgs/system.nix
@@ -2,24 +2,19 @@
 
 with pkgs;
 [
-  neovim
   vim
-  tmux
   putty
   gedit
   terminator
   pcmanfm
   capture
-  bat
   virtualbox
   home-manager
   typora
   zotero
-  eza
   ulauncher
   xscreensaver
   btop
   htop
   nix-bash-completions
-
 ]
