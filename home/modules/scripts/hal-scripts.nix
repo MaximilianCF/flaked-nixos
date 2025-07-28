8407afcfddee4d6b8e97ed699364487c3aa19d1e
Maximilian Canez Fernandes <92341177+MaximilianCF@users.noreply.github.com>
2025-07-27 22:46:10 -0300

    Update hal-scripts.nix

--- home/modules/scripts/hal-scripts.nix
+++ home/modules/scripts/hal-scripts.nix
@@ -1,1 +1,24 @@
+{ config, lib, pkgs, ... }:
+
+let
+  scripts = [
+    "nixos-upgrade.sh"
+    "devshell-list.sh"
+    "flake-diff.sh"
+    "nix-gc-safe.sh"
+  ];
+in {
+  options.max.scripts.hal.enable = lib.mkEnableOption "Habilita o pacote HAL de scripts Nix/NixOS";
+
+  config = lib.mkIf config.max.scripts.hal.enable {
+    home.packages = with pkgs; [ fzf jq ];
+
+    xdg.configFile = builtins.listToAttrs (map (name: {
+      name = "bin/${lib.removeSuffix ".sh" name}";
+      value.source = ../../scripts/${name};
+    }) scripts);
+
+    home.sessionPath = [ "$HOME/.config/bin" ];
+  };
+}
 
