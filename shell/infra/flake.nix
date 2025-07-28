a2d0655cfc0280fc107e7e90e8a3a087ca1d8356
Maximilian Canez Fernandes <92341177+MaximilianCF@users.noreply.github.com>
2025-07-27 21:36:53 -0300

    Update flake.nix

--- shell/infra/flake.nix
+++ shell/infra/flake.nix
@@ -1,1 +1,32 @@
+{
+  description = "DevShell para infraestrutura declarativa com Terraform e Colmena";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "infra-dev-shell";
+          buildInputs = with pkgs; [
+            git
+            terraform
+            opentofu
+            colmena
+            sops
+            age
+            jq
+            yq
+          ];
+          shellHook = ''
+            echo "🔧 Shell de infraestrutura ativado. Ready to terraform apply!"
+          '';
+        };
+      });
+}
 
