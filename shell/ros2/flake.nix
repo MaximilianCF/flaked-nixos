718015cd53a9745d0168ebde1427b7d2e505f3ed
Maximilian Canez Fernandes <92341177+MaximilianCF@users.noreply.github.com>
2025-07-27 21:37:12 -0300

    Update flake.nix

--- shell/ros2/flake.nix
+++ shell/ros2/flake.nix
@@ -1,1 +1,36 @@
+{
+  description = "DevShell para simulações e pacotes ROS2";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, nix-ros-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ nix-ros-overlay.overlays.default ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          config.allowUnfree = true;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "ros2-dev-shell";
+          buildInputs = with pkgs; [
+            git
+            colcon
+            rosPackages.ros2Control
+            rosPackages.ros2cli
+            rosPackages.ros2Launch
+            gazebo
+            rviz
+          ];
+          shellHook = ''
+            echo "🤖 Ambiente ROS2 ativo! Rode: colcon build && source install/setup.bash"
+          '';
+        };
+      });
+}
 
