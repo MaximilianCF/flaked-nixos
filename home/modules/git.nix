314d01a4b4032ba003de32e2c1a08fb8fec95f97
Max <maximiliancf.dev@icloud.com>
2025-08-13 16:49:00 -0300

    feat: New features.

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -8,6 +8,18 @@
     lfs = {
       enable = true;
     };
+    delta = {
+      enable = true;
+      package = pkgs.delta;
+      options = {
+        decorations = {
+          commit-decoration-style = "bold yellow box ul";
+          file-decoration-style = "none";
+          file-style = "bold yellow ul";
+        };
+        features = "decorations";
+        whitespace-error-style = "22 reverse";
+      };
+    };
   };
-
 }
