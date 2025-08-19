e59b35030eddcb558851e4de18afb5b3bf49f6a0
Max <maximiliancf.dev@icloud.com>
2025-08-19 10:27:18 -0300

    feat: Virtualization added.

--- hosts/tars.nix
+++ hosts/tars.nix
@@ -7,6 +7,8 @@
 
   virtualisation.virtualbox.host.enable = true;
   virtualisation.virtualbox.host.enableExtensionPack = true;
+  virtualisation.virtualbox.guest.enable = true;
+  virtualisation.virtualbox.guest.dragAndDrop = true;
 
   boot.loader = {
     grub = {
