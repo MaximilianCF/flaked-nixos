bb84e829dd3336dbcebcbf6168301f17ae9b56c1
Max <maximiliancf.dev@icloud.com>
2025-08-23 14:50:00 -0300

    feat: Octave and packages

--- home/pkgs/robotica.nix
+++ home/pkgs/robotica.nix
@@ -4,6 +4,15 @@ with pkgs;
 [
   #foxglove-cli
   g2o
-  octave
+  octaveFull
+  octavePackages.statistics
+  octavePackages.control
+  octavePackages.signal
+  octavePackages.optim
+  octavePackages.linear-algebra
+  octavePackages.financial
+  octavePackages.io
+  octavePackages.image
+  octavePackages.data-smoothing
 
 ]
--- hosts/hal9000.nix
+++ hosts/hal9000.nix
@@ -136,6 +136,22 @@
     '';
   };
 
+  xdg.portal = {
+    enable = true;
+    # Para GNOME:
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gnome
+      pkgs.xdg-desktop-portal-gtk
+    ];
+    # Se estiver no KDE, troque para: [ pkgs.xdg-desktop-portal-kde ]
+    # Se estiver em Sway/Wayland puro, use: [ pkgs.xdg-desktop-portal-wlr pkgs.xdg-desktop-portal-gtk ]
+  };
+
+  # Opcional: deixar Qt alinhado ao GNOME fora dos flatpaks
+  environment.systemPackages = [
+    pkgs.qgnomeplatform-qt6
+  ];
+
   users.users.max = {
     packages = with pkgs; [
       flatpak
