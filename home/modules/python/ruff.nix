1424946a1c6f0d7354efae018623ee3dd638f8dc
Max <maximiliancf.dev@icloud.com>
2025-09-04 17:06:09 -0300

    fix: Ruff linter

---
+++ home/modules/python/ruff.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+
+{
+
+  programs.ruff = {
+    enable = true;
+    package = pkgs.ruff;
+    settings = {
+      line-length = 100;
+      per-file-ignores = {
+        "__init__.py" = [ "F401" ];
+      };
+      lint = {
+        select = [
+          "E4"
+          "E7"
+          "E9"
+          "F"
+        ];
+        ignore = [ ];
+      };
+    };
+  };
+
+}
